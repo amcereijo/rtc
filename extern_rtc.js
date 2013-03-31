3da6d557444371c089c4f683773eabd41f831b8d
amcereijo <amcereijo@gmail.com>
2013-03-31 21:31:47 +0200

    code correct control not apply plugin profiile page

--- extern_rtc.js
+++ extern_rtc.js
@@ -17,8 +17,15 @@ var ExternFunction = (function() {
 		//twitter user
 		var twitterUser = ' RT @' + $(parent).find('span.username').find('b').text()+ ' ';
 
+		//remove click function when click open "new tweet" element
+		$('#global-new-tweet-button').unbind('click');
+
 		//click to open new tweet modal
 		$('#global-new-tweet-button').click();
+
+		//add click function when click open "new tweet" element
+		$('#global-new-tweet-button').click(clickNewTweet);
+
 		//save previus title
 		previusTitle = $('#global-tweet-dialog').find('.modal-title').text();
 		//change modal title
@@ -29,36 +36,36 @@ var ExternFunction = (function() {
 		$('#global-tweet-dialog').find('.rtcTitle').removeClass('modal-title');
 		//center new title
 		$('.rtcTitle').css('text-align','center');
-
-		//add click function whe click open new tweet element
-		$('#global-new-tweet-button').click(function () {
-			//remove center title
-			$('.rtcTitle').css('text-align','');
-			//add original title class
-			$('#global-tweet-dialog').find('.rtcTitle').addClass('modal-title');
-			//remove temporal "rtc" title class
-			$('#global-tweet-dialog').find('.modal-title').removeClass('rtcTitle');
-			//set original title
-			$('#global-tweet-dialog').find('.modal-title').text(previusTitle);
-			//remove retweet text
-			$('#tweet-box-global').find('div').text('');
-		});
+		
 		//add retweet text
 		$('#tweet-box-global').find('div').text(twitterUser+tweetText);
 	};
 
+	//function to set original title, class and empty content
+	var clickNewTweet = function () {
+		//remove center title
+		$('.rtcTitle').css('text-align','');
+		//add original title class
+		$('#global-tweet-dialog').find('.rtcTitle').addClass('modal-title');
+		//remove temporal "rtc" title class
+		$('#global-tweet-dialog').find('.modal-title').removeClass('rtcTitle');
+		//set original title
+		$('#global-tweet-dialog').find('.modal-title').text(previusTitle);
+		//remove retweet text
+		//$('#tweet-box-global').find('div').text('');
+		$('#tweet-box-global').find('div').empty();
+	};
 
 	//function for page tree modification
 	var treeModifi = function () {
 		if($('ul.tweet-actions:not(.rtc)').length !== 0){
-			public.addOption();
+			addOption();
 		}
 	};
 
-	//public methods
-	var public = {
-		//function to add li rtc elements and its actions
-		addOption : function () {
+	//function to add li rtc elements and its actions
+	var addOption = function () {
+		if($('ul.tweet-actions:not(.rtc)').length>0){
 			//remove tree listener while we modify the page content
 			$('.stream-container').unbind("DOMSubtreeModified");
 			//finde elements with no rtc li option
@@ -70,9 +77,20 @@ var ExternFunction = (function() {
 			//add tree listener to know when we have to add more rtc li elements
 			$('.stream-container').bind("DOMSubtreeModified",treeModifi);
 		}
+	};
+
+	//public methods
+	var public = {
+		initPlugin : function () {
+			//avoid profile page
+			if($('.profile.active').length===0){
+				addOption();
+			}
+		}
 	}
 	return public;
 })();
+
 //execute when load
-ExternFunction.addOption();
+ExternFunction.initPlugin();
 
