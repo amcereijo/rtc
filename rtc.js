5a59a8cc302cdfd85d36b1fa36e9b8f5b251f03c
amcereijo <amcereijo@gmail.com>
2013-04-04 11:47:48 +0200

    removed update listener

--- rtc.js
+++ rtc.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 	var filters = {
 	  url: [{urlContains: "https://twitter.com/".replace(/^https?\:\/\//, '')}]
 	};
-	
+
 	//listener for pages (only twitter using "filters" var)
 	chrome.webNavigation.onCompleted.addListener(
 		function(){
@@ -15,13 +15,4 @@ $(document).ready(function () {
 		}
 		,filters
 	);
-
-	chrome.tabs.onUpdated.addListener(function(tab_id, changeInfo, tab) {
-		//alert(changeInfo.status + ','+changeInfo.url.indexOf('/twitter.com/'));
-	    if(changeInfo.status == 'loading' && changeInfo.url) {
-	    	chrome.tabs.executeScript(null, {file: "extern_rtc.js"});
-	        
-	    }
-	});
-
 });
